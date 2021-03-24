ca47f68bfe196a4bae9d924714bf56520d3a464c
simplly <takuyax0806@docomo.ne.jp>
2021-03-24 07:09:07 +0000

    finish

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,10 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :cart_items, dependent: :destroy
